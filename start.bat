f1ee56579c9f8d805f4ce0da76c12375003bcfca
xanan <xanan@yandex.ru>
2023-05-18 14:42:56 +0300

    adding updating

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 echo "Welcome to Minecraft server setup"
 echo "Building & Running Minecraft server..."
+CMD /c "git pull"
 CMD /c "docker compose up -d && docker attach minecraft-server"
 echo "Exiting setup..."
 
